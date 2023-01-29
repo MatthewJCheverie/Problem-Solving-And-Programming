89f28072443d0794bf9539decae0c35090593814
Matthew Cheverie <matthewjohncheverie@gmail.com>
2023-01-28 22:37:22 -0500

    Assignment Four

---
+++ AssignmentFour/AssignmentFour.py
@@ -0,0 +1,7 @@
+PizzaPrice=10.50
+print("Number of Pizzas   Total Cost")
+for NumberOfPizzas in range(1,10): 
+    Cost=PizzaPrice*NumberOfPizzas
+    print("Number of Pizzas: ", NumberOfPizzas, "Total Cost: ", Cost)
+else:    
+      print("**End**")
